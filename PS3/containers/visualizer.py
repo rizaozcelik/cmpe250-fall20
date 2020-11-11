398583a3e479bcef570e972e2404040e8b91c92e
Riza Ozcelik <rizaozcelik96@gmail.com>
2020-11-11 09:20:05 +0300

    PS3-containers minor figure updates

--- PS3/containers/visualizer.py
+++ PS3/containers/visualizer.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 def preprocess_data(container, func):
     times = np.loadtxt(f'./measurements/{container}_{func}.txt')
-    avg = np.mean(times, axis=0)
+    avg = np.median(times, axis=0)
     return pd.Series(avg).rolling(10).median()
 
 
@@ -18,7 +18,7 @@ deque = preprocess_data('deque', func)
 vector = preprocess_data('vector', func)
 
 fig, ax1 = plt.subplots(figsize=(12, 4))
-ax1.set_xlabel('Iteration')
+ax1.set_xlabel('Container Size')
 ax1.set_ylabel('Nanoseconds - List & Deque')
 ax1.plot(list(range(len(lst))), lst, color='orange', label='List')
 ax1.plot(list(range(len(deque))), deque, color='blue', label='Deque')
@@ -30,7 +30,7 @@ ax2.plot(list(range(len(vector))), vector, color='red', label='Vector')
 fig.tight_layout()
 ax2.legend()
 ax1.legend(loc='center left')
-plt.title(f'Comparison of containers for ${func}$')
+plt.title(f'Comparison of containers for ${func}$ -- Vector is not a suitable choice')
 plt.savefig(f'./figures/{func}', bbox_inches='tight')
 plt.show()
 #%%
@@ -41,15 +41,16 @@ deque = preprocess_data('deque', func)
 vector = preprocess_data('vector', func)
 
 plt.figure(figsize=(12, 4))
-plt.xlabel('Iteration')
+plt.xlabel('Container Size')
 plt.ylabel('Nanoseconds')
+plt.ylim(0, 200)
 plt.plot(list(range(len(lst))), lst, color='orange', label='List')
 plt.plot(list(range(len(deque))), deque, color='blue', label='Deque')
 plt.plot(list(range(len(vector))), vector, color='red', label='Vector')
 
 plt.tight_layout()
 plt.legend()
-plt.title(f'Comparison of containers for ${func}$')
+plt.title(f'Comparison of containers for ${func}$ -- Constant of list is higher')
 plt.savefig(f'./figures/{func}', bbox_inches='tight')
 plt.show()
 #%%
@@ -58,12 +59,13 @@ func = 'middle'
 lst = preprocess_data('list', func)
 deque = preprocess_data('deque', func)
 vector = preprocess_data('vector', func)
-
+#%%
 fig, ax1 = plt.subplots(figsize=(12, 4))
-ax1.set_xlabel('Iteration')
+ax1.set_xlabel('Container Size')
 ax1.set_ylabel('Nanoseconds - List & Vector')
 ax1.plot(list(range(len(lst))), lst, color='orange', label='List')
 ax1.plot(list(range(len(vector))), vector, color='red', label='Vector')
+ax1.set_ylim(0, 75000)
 
 ax2 = ax1.twinx()
 ax2.set_ylabel('Nanoseconds - Deque')
@@ -71,7 +73,7 @@ ax2.plot(list(range(len(deque))), deque, color='blue', label='Deque')
 
 fig.tight_layout()
 ax2.legend(loc='center left')
-ax1.legend()
-plt.title(f'Comparison of containers for ${func}$ insertion')
+ax1.legend(loc='upper left')
+plt.title(f'Comparison of containers for ${func}$ insertion -- List is the way to go')
 plt.savefig(f'./figures/{func}', bbox_inches='tight')
 plt.show()
